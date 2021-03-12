acc212ffa105f1950786cd75ebd53634bbe1a989
wangtx <wang.tianxia@foxmail.com>
2021-03-12 15:56:26 +0800

    use unshare to land the forked process in a new container

--- python-base-image/daemon.py
+++ python-base-image/daemon.py
@@ -2,6 +2,8 @@ import traceback
 import json
 import os
 import time
+import ol
+import uuid
 
 import tornado
 import tornado.ioloop
@@ -13,7 +15,7 @@ file_sock_path = 'fork.sock'
 file_sock = None
 
 def start_app_server():
-    print("daemon.py: start app server on fd: %d" % file_sock.fileno())
+    # print("daemon.py: start app server on fd: %d" % file_sock.fileno())
 
     class SockFileHandler(tornado.web.RequestHandler):
         def post(self):
@@ -41,35 +43,48 @@ def start_app_server():
 def start_fork_server():
     global file_sock
     global file_sock_path
-    print("daemon.py: start fork server on fd: %d" % file_sock.fileno())
+    # print("daemon.py: start fork server on fd: %d" % file_sock.fileno())
     file_sock.setblocking(True)
 
     while True:
         client, info = file_sock.accept()
-        start = time.perf_counter_ns()
+        # start = time.perf_counter_ns()
+        # rv = ol.unshare()
+        # assert rv == 0
         pid = os.fork()
-        end = time.perf_counter_ns()
+        # end = time.perf_counter_ns()
 
         if pid:
-            client.sendall(bytes(str(pid), 'utf8'))
-            client.sendall(bytes('\n', 'utf8'))
-            client.sendall(bytes(str(end-start), 'utf8'))
-            client.sendall(bytes('ns', 'utf8'))
-            client.sendall(bytes('\n', 'utf8'))
+            # the grand-parent process
+            # ret, exitcode = os.waitpid(pid, 0)
+            # end = time.perf_counter_ns()
+            # print('waitpid returns %d %d' % (ret, exitcode))
+            # client.sendall(bytes(str(pid), 'utf8'))
             client.close()
         else:
-            file_sock.close()
-            file_sock = None
+            # the parent process
+            rv = ol.unshare()
+            assert rv == 0
+            pid = os.fork()
+            if pid:
+                # the parent process
+                client.sendall(bytes(str(pid), 'utf8'))
+                client.close()
+                os._exit(0)
+            else:
+                # the child process
+                file_sock.close()
+                file_sock = None
 
-            file_sock_path = 'fork.sock.' + str(os.getpid())
-            file_sock = tornado.netutil.bind_unix_socket(file_sock_path)
-            client.close()
-            start_fork_server()
+                file_sock_path = 'fork.sock.' + str(os.getpid()) + '.' + str(uuid.uuid4())
+                file_sock = tornado.netutil.bind_unix_socket(file_sock_path)
+                client.close()
+                start_fork_server()
 
 
 def main():
     global file_sock
-    print("daemon.py: main")
+    # print("daemon.py: main")
     file_sock = tornado.netutil.bind_unix_socket(file_sock_path)
     start_fork_server()
 
