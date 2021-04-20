2286ae4b68a8dcffc2c40da55e7042dbdd7e3d04
Lu Jun <lujun7460@csu.edu.cn>
2021-04-20 08:01:42 -0500

    update bundle_path on bluefield
    Signed-off-by: Lu Jun <lujun7460@csu.edu.cn>

--- scripts/run_baseline_loop.sh
+++ scripts/run_baseline_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./config
-BUNDLE_PATH=~/.base/baseline
+BUNDLE_PATH=/run/.base/baseline
 cd $BUNDLE_PATH
 cp config-loop.json config.json
 sudo $RUNC run -d baseline-$1 > /dev/null 2>&1
