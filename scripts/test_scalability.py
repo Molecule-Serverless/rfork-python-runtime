46e0955ca687b71c0fd0aa2e40774fd7fdda51e3
Dong Du <ddnirvana1@gmail.com>
2021-04-21 10:09:47 +0800

    update scripts to log both startup and end2end latency
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

--- scripts/test_scalability.py
+++ scripts/test_scalability.py
@@ -9,6 +9,8 @@ USAGE="python3 test_baseline.py [test], test can be \"baseline\" or \"fork\"\nIf
 def test_fork_start():
     latencies = []
     all_latencies = []
+    e2e_latencies = []
+    e2e_all_latencies = []
     ENDPOINT_BUNDLE="%s/.base/spin0/rootfs" %os.environ['HOME']
     COMMAND_FORK = "./run_fork.sh"
     COMMAND_LOOP_RUN = "./run_baseline_loop.sh %d"
@@ -23,15 +25,23 @@ def test_fork_start():
         output_line_child = open(ENDPOINT_BUNDLE + "/log.txt", "r").read()
         output_lines.append(output_line_child)
         # print(output_lines)
-        invokeTime, startTime = parse_output_lines(output_lines, "fork")
+        invokeTime, startTime, retTime = parse_output_lines(output_lines, "fork")
         # print(invokeTime, startTime)
         start_latency = startTime - invokeTime
+        e2e_latency = retTime - invokeTime # End2End latency
+
         latencies.append(start_latency)
         all_latencies.append(start_latency)
-        format_result(latencies, "fork")
+        e2e_latencies.append(e2e_latency)
+        e2e_all_latencies.append(e2e_latency)
+
+        format_result(latencies, "fork-startup")
+        format_result(e2e_latencies, "fork-end2end")
         os.system(COMMAND_LOOP_RUN % i)
         latencies = []
-        format_scale_result(all_latencies, "fork")
+        format_scale_result(all_latencies, "fork-startup")
+        e2e_latencies = []
+        format_scale_result(e2e_all_latencies, "fork-end2end")
     # print(latencies)
 
 # pre-requisite: finish the building of the baseline container bundle, i.e., ~/.base/baseline/rootfs and ~/.base/baseline/config.json
@@ -39,6 +49,8 @@ def test_baseline_start():
     # PWD must be in scripts/tests
     latencies = []
     all_latencies = []
+    e2e_latencies = []
+    e2e_all_latencies = []
     COMMAND_RUN = "./run_baseline.sh %d"
     COMMAND_LOOP_RUN = "./run_baseline_loop.sh %d"
 
@@ -46,25 +58,37 @@ def test_baseline_start():
         exec_ = os.popen(COMMAND_RUN %i)
         output_lines = exec_.read().strip().split('\n')
         # print(output_lines)
-        invokeTime, startTime = parse_output_lines(output_lines, "baseline")
+        invokeTime, startTime, retTime = parse_output_lines(output_lines, "baseline")
         start_latency = startTime - invokeTime
+        e2e_latency = retTime - invokeTime # End2End latency
+
         latencies.append(start_latency)
         all_latencies.append(start_latency)
-        format_result(latencies, "baseline")
+
+        e2e_latencies.append(e2e_latency)
+        e2e_all_latencies.append(e2e_latency)
+        format_result(latencies, "baseline-startup")
+        format_result(e2e_latencies, "baseline-end2end")
+
         os.system(COMMAND_LOOP_RUN % i)
         latencies = []
-        format_scale_result(all_latencies, "fork")
+        format_scale_result(all_latencies, "baseline-startup")
+        e2e_latencies = []
+        format_scale_result(e2e_all_latencies, "baseline-end2end")
+        #print(start_latency, e2e_latency)
     # print(latencies)
 
 
 def parse_output_lines(output_lines, test):
     invokeTime_pattern_line = TEST_INVOKETIME_PATTERN[test]
     startTime_pattern_line = "\'startTime\': 1[0-9]{12}"
+    retTime_pattern_line = "\'retTime\': 1[0-9]{12}"
     time_pattern = "1[0-9]{12}"
 
     # Find lines that contains the invokeTime and the startTime
     invokeTime_line = None
     startTime_line = None
+    retTime_line = None
     for line in output_lines:
         invokeTime_match = re.search(invokeTime_pattern_line, line)
         if invokeTime_match != None:
@@ -77,17 +101,27 @@ def parse_output_lines(output_lines, test):
             # print(line)
             startTime_line = line
 
-    if startTime_line == None or invokeTime_line == None:
+        retTime_match = re.search(retTime_pattern_line, line)
+        if retTime_match != None:
+            # print(line)
+            retTime_line = line
+
+
+    if startTime_line == None or invokeTime_line == None or retTime_line == None:
         print("error output: can't find the startTime or invokeTime")
         exit()
     # Find the time value in the lines
     invokeTime_search = re.search(time_pattern, invokeTime_line)
     startTime_search = re.search(startTime_pattern_line, startTime_line)
+    retTime_search = re.search(retTime_pattern_line, retTime_line)
 
     invokeTime = invokeTime_line[invokeTime_search.span()[0]: invokeTime_search.span()[0] + 13]
     startTime = startTime_line[startTime_search.span()[1] - 13 : startTime_search.span()[1]]
+    retTime = startTime_line[retTime_search.span()[1] - 13 : retTime_search.span()[1]]
 
-    return int(invokeTime), int(startTime)
+    #print(startTime, retTime)
+
+    return int(invokeTime), int(startTime), int(retTime)
 
 def format_result(latencies, test):
     request_num = len(latencies)
