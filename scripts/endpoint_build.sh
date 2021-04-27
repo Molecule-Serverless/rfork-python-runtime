af1535189f9c8c550a4077379e1ffd3539415f0f
CodingYuanLiu <lqyuan980413@163.com>
2021-04-27 15:55:53 +0800

    build scripts for spin image and python endpoint image

--- scripts/endpoint_build.sh
+++ scripts/endpoint_build.sh
@@ -2,7 +2,8 @@
 BUNDLE_PATH=~/.base/spin0
 
 cd ../spin-base-image 
-gcc spin.c -o spin -O2 -static && docker build -t spin-base-image .
+gcc spin.c -o spin -O2 -static && docker build -t spin-base-image -f Dockerfile .
+docker build -t python-endpoint-base -f python_endpoint.Dockerfile .
 sudo rm -rf $BUNDLE_PATH/rootfs
 sudo mkdir -p $BUNDLE_PATH/rootfs
 
@@ -10,4 +11,4 @@ if [[ ! -f "$BUNDLE_PATH/config.json" ]]; then
     echo "Cannot find config.json. Paste a new one"
     cp config.json $BUNDLE_PATH
 fi
-sudo docker export `docker create spin-base-image` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
+sudo docker export `docker create python-endpoint-base` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
