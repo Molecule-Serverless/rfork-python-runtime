54770629e35bf2dcbe8ebcfc690f4cb2a128547a
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-22 15:38:25 +0800

    scripts: update scripts to use prepared (prebuilt) docker images for test
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

---
+++ scripts/app_template_build_prebuilt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+BUNDLE_PATH=~/.base/container0
+
+if [ $# -eq 0 ]
+then
+	echo "Please pass the app (baseline) name first!"
+	exit -1
+fi
+
+docker pull $1 >/dev/null 2>&1
+sudo rm -rf $BUNDLE_PATH/rootfs
+sudo mkdir -p $BUNDLE_PATH/rootfs
+if [[ ! -f "$BUNDLE_PATH/config-base.json" ]]; then
+    echo "Cannot find config.json. Paste a new one"
+    cp config.json $BUNDLE_PATH/config-base.json
+    cp config-loop.json $BUNDLE_PATH/config-loop.json
+fi
+sudo docker export `docker create $1` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
