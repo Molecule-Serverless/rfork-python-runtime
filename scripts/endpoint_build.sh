cd7584a8d7cd22f41e998563291dbb97a229030f
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-27 15:05:30 +0800

    refine scripts for cfork tests
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- scripts/endpoint_build.sh
+++ scripts/endpoint_build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 BUNDLE_PATH=~/.base/spin0
 
-cd ../spin-base-image 
+cd ../spin-base-image
 gcc spin.c -o spin -O2 -static && docker build -t spin-base-image .
 sudo rm -rf $BUNDLE_PATH/rootfs
 sudo mkdir -p $BUNDLE_PATH/rootfs
 
 if [[ ! -f "$BUNDLE_PATH/config.json" ]]; then
     echo "Cannot find config.json. Paste a new one"
-    cp config.json $BUNDLE_PATH
+    cp configs/spin0/config.json $BUNDLE_PATH
 fi
 sudo docker export `docker create spin-base-image` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
--- scripts/template_build.sh
+++ scripts/template_build.sh
@@ -7,7 +7,7 @@ sudo rm -rf $BUNDLE_PATH/rootfs
 sudo mkdir -p $BUNDLE_PATH/rootfs
 if [[ ! -f "$BUNDLE_PATH/config-base.json" ]]; then
     echo "Cannot find config.json. Paste a new one"
-    cp config.json $BUNDLE_PATH/config-base.json
-    cp config-loop.json $BUNDLE_PATH/config-loop.json
+    cp configs/container0/config.json $BUNDLE_PATH/config-base.json
+    cp configs/container0/config-loop.json $BUNDLE_PATH/config-loop.json
 fi
 sudo docker export `docker create python-base-image` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
