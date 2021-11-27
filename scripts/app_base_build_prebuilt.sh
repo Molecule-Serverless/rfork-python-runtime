0bbfa50b56820ce482893de6c1b984429863a12d
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-27 14:20:34 +0800

    add config files
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- scripts/app_base_build_prebuilt.sh
+++ scripts/app_base_build_prebuilt.sh
@@ -12,7 +12,7 @@ sudo rm -rf $BUNDLE_PATH/rootfs
 sudo mkdir -p $BUNDLE_PATH/rootfs
 if [[ ! -f "$BUNDLE_PATH/config-base.json" ]]; then
     echo "Cannot find config.json. Paste a new one"
-    cp config.json $BUNDLE_PATH/config-base.json
+    cp configs/baseline/config.json $BUNDLE_PATH/config-base.json
     #cp config-loop.json $BUNDLE_PATH
 fi
 sudo docker export `docker create $1` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
--- scripts/app_endpoint_build_prebuilt.sh
+++ scripts/app_endpoint_build_prebuilt.sh
@@ -13,6 +13,6 @@ sudo mkdir -p $BUNDLE_PATH/rootfs
 
 if [[ ! -f "$BUNDLE_PATH/config.json" ]]; then
     echo "Cannot find config.json. Paste a new one"
-    cp config.json $BUNDLE_PATH
+    cp configs/spin0/config.json $BUNDLE_PATH
 fi
 sudo docker export `docker create $1` | sudo tar -C $BUNDLE_PATH/rootfs -xf -
